714d26e0e2cd0035da48efe2abc17067b8db0729
Samuel B. Johnson <sabjohnso@yahoo.com>
2021-12-30 14:51:07 -0600

    :bug: [Basic Graph] Add missing rackunit-typed dependency
    Problem:
    - The typed rackunit library is being used but was not listend in the
      dependencies
    
    Solution:
    - Add rackunit-typed to the dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "graph")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/graph.scrbl" ())))
 (define pkg-desc "Data structures, algorithms and languages for graphs.")
 (define version "0.0")
